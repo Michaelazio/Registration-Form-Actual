783ebb205e4dbd92ac228c8ddced60262aac5a48
Michaelazio <kunalmukherjee12@gmail.com>
2024-07-20 11:12:32 +0530

    2nd

--- login.js
+++ login.js
@@ -3,8 +3,12 @@ const loginEmail = document.getElementById("login-email");
 const loginPass = document.getElementById("login-pass");
 const rememberMe = document.getElementById("remember-me");
 
-let savedPassword = JSON.parse(localStorage.getItem("formData")).password;
-console.log(savedPassword);
+// let savedPassword = JSON.parse(localStorage.getItem("formData")).password;
+// console.log(savedPassword);
+
+let canLogin = false;
+
+console.log(rememberMe);
 
 loginForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
@@ -16,6 +20,7 @@ loginForm.addEventListener("submit", function (evt) {
         type: el.type,
         value: el.value.trim(),
         required: el.required,
+        checked: el.checked,
       };
     });
   console.log(getFormData);
@@ -26,9 +31,33 @@ loginForm.addEventListener("submit", function (evt) {
   let passMatch = passwordRegEx.test(
     getFormData.find((el) => el.name === "password").value
   );
-  let matchSavePassword =
-    JSON.parse(window.localStorage.getItem("formData")).password ===
-    getFormData.find((el) => el.name === "password").value;
+  
+  let isAllowed = isEmVal && passMatch ? (canLogin = true) : (canLogin = false);
+  if (isAllowed) {
+    let loginData = {};
+    if (getFormData.find((obj) => obj.type === "checkbox").checked === true) {
+      loginData = {
+        email: getFormData.find((el) => el.type === "email").value,
+        password: getFormData.find((el) => el.name === "password").value,
+        checked: getFormData.find(el => el.type === 'checkbox').checked
+      };
+      localStorage.setItem("loginData", JSON.stringify(loginData));
+    }
+    return alert(`You can Successfuly login`);
+  } else {
+    return alert(`oops!! credential's didn't match`);
+  }
 });
 
-function loginClick() {}
+(()=>{
+  let isChecked = JSON.parse(localStorage.getItem('loginData')).checked; 
+  
+
+    if(isChecked){
+      loginEmail.value = JSON.parse(localStorage.getItem('loginData')).email;
+      loginPass.value = JSON.parse(localStorage.getItem('loginData')).password
+      rememberMe.checked = true
+    }
+
+
+})()
